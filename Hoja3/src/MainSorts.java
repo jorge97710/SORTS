1b0c13c5c78c5adf4629ffa292f9abc88e11c7f9
jorge97710 <jazmitia_10@hotmail.com>
2016-07-31 00:07:32 -0600

    v 1.3 otra vez

--- Hoja3/src/MainSorts.java
+++ Hoja3/src/MainSorts.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -19,6 +20,8 @@ public class MainSorts {
 	/**
 		 * Ejecuta el programa.
 		 */
+	
+	
 	static String textoarray="",txt="";
     static Stack<String> miStack = new Stack<String>();
 static ArrayList<String> array = new ArrayList<String>();
@@ -58,6 +61,7 @@ array.add(texto);
 	    	 lista[i]=Integer.parseInt(array.get(i));
 	    	 
 	     }
+	     
 
 		// -------------------------------Integer[] ia = {30,1000,0, 20, 10, 5, 6, 99};
 	     //quickSORT
@@ -69,9 +73,9 @@ array.add(texto);
 	     //       System.out.println(i);
 	     //   }
 		 
-		 /* Merge
-			Integer[] a = {30,1000,0, 20, 10, 5, 6, 99};
-	                mergeSort(a);
+		 // Merge
+/*			Integer[] a = {30,1000,0, 20, 10, 5, 6, 99};
+			q.GNOMESORT(a);
 			System.out.println(Arrays.toString(a));*/
 	 } 
 }
